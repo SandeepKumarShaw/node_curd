0126f64206de019d3218e5bd14264ef9916c6b04
sandeeptier5 <sandy.shaw20@gmail.com>
2018-07-10 00:34:18 +0530

    V 2.0 stable released to dev

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 //template engine
 app.set('view engine','ejs');
 //routes
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,28 @@
 var express = require('express');
+var multer = require('multer');
+
 var app = express();
+
+var multerConf = {
+    storage : multer.diskStorage({
+      destination: function(req,file,next){
+        next(null,'./public/images');
+      },
+    filename:function(req,file,next){
+        var originalname = file.originalname;
+        var extension = originalname.split(".");
+        filename = Date.now() + '.' + extension[extension.length-1];
+        next(null, filename);
+    },
+    fileFilter:function(req,file,next){
+       if(!file){
+        next(null, false)
+       }else{
+        next(null, true)
+       }
+    }  
+    })
+}
 
 //Connecting to the Database
 
@@ -11,9 +34,9 @@ mongoose.connect("mongodb://localhost:27017/node-demo");
 //Creating a Database Schema
 var nameSchema = new mongoose.Schema({
     firstName: String,
-    lastName: String
+    lastName: String,
+    photo: String
 });
-
 var User = mongoose.model("User", nameSchema);
 
 //home route
@@ -26,13 +49,17 @@ app.get('/',function(req,res){
     })
 });
 //add route
-app.post('/',function(req,res){
+app.post('/', multer(multerConf).single('photo'), function(req,res){
+
+    if(req.file){
+        console.log();
+        req.body.photo = req.file.filename;
+    }
+    
    req.assert('firstName','First Name is required').notEmpty();
    req.assert('lastName','Last Name is required').notEmpty();
+   req.assert('photo','Image required').notEmpty();
 
-   /*  req.checkBody("firstName", "First Name is required.").notEmpty();
-    req.checkBody("lastName", "Last Name is required.").notEmpty();
- */
     var errors = req.validationErrors();
     if (!errors) {
         var myData = new User(req.body);
@@ -44,7 +71,8 @@ app.post('/',function(req,res){
               res.render('add', {
                 title: 'Add New User',
                 firstName: user.firstName,
-                lastName: user.lastName            
+                lastName: user.lastName,
+                photo:req.body.photo            
                });
         });
     } else {
@@ -57,7 +85,8 @@ app.post('/',function(req,res){
         res.render('add', { 
             title: 'Add New User',
             firstName: req.body.firstName,
-            lastName: req.body.lastName
+            lastName: req.body.lastName,
+            photo:req.body.photo    
         })
     } 
 
@@ -80,17 +109,6 @@ app.get('/edit/:user_id',function(req,res){
     if (err)
         res.send(err);
     else
-        res.render('edit', {results: result});
-   });
-});
-
-//update route
-app.get('/edit/:user_id',function(req,res){
-    let id = req.params.user_id;
-   User.findOne({_id : id},function(err,result){
-    if (err)
-        res.send(err);
-    else
         res.render('edit', {            
                 title: 'Edit User',
                 results:result       
@@ -101,16 +119,24 @@ app.get('/edit/:user_id',function(req,res){
 });
 
 //update route
-app.post('/edit/:user_id', function(req, res) {   
+app.post('/edit/:user_id',multer(multerConf).single('photo'), function(req, res) {   
+
+    if(req.file){
+        console.log();
+        req.body.photo = req.file.filename;
+    }
 
     req.assert('firstName','First Name is required').notEmpty();
     req.assert('lastName','Last Name is required').notEmpty();
+    //req.assert('photo','Image required').notEmpty();
+
     var errors = req.validationErrors();
     if (!errors) {
         let id = req.params.user_id;
         var data = {
             firstName : req.body.firstName,
-            lastName : req.body.lastName
+            lastName : req.body.lastName,
+            photo:req.body.photo 
         }
     
         // save the user
